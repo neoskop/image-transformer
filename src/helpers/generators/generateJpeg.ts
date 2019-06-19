2ed91dea58eafa6fe0e0139e358fbabcdcb25fe2
Yannik Wissner <wissner@neoskop.de>
2019-06-19 09:55:13 +0200

    fix: keep jp(e)g extension of source file

--- src/helpers/generators/generateJpeg.ts
+++ src/helpers/generators/generateJpeg.ts
@@ -3,14 +3,20 @@ import sharp from 'sharp';
 import { Size } from '../../types/config';
 import { changeExtension } from '../changeExtension';
 import { GeneratorParams } from './interfaces';
+import path from 'path';
 
 export const generateJpeg = async (params: GeneratorParams, size: Size) => {
   const { filePath, targetFilepath, opts } = params;
-  const jpegFilePath = changeExtension(targetFilepath, `${size.suffix}.jpeg`);
+  const extname = path.extname(filePath);
+
+  const jpegFilePath = changeExtension(
+    targetFilepath,
+    `${size.suffix}${extname === 'jpg' ? 'jpg' : 'jpeg'}`
+  );
 
   if (!fs.existsSync(jpegFilePath)) {
     sharp(filePath)
-      .resize(size.width, size.height, {withoutEnlargement: true})
+      .resize(size.width, size.height, { withoutEnlargement: true })
       .jpeg(opts.formats.jpg)
       .toFile(jpegFilePath);
   }
