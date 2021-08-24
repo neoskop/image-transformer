bbd95a80f9931ed5ea68ee0149df395fa57e04fc
SimonSiefkeWork <89010006+SimonSiefkeWork@users.noreply.github.com>
2021-08-24 12:33:47 +0000

    update dependencies, hope to fix bugs

--- src/helpers/optimizeSvg.ts
+++ src/helpers/optimizeSvg.ts
@@ -1,10 +1,8 @@
-import SVGO from 'svgo';
+import { optimize } from 'svgo';
 import { OptimizedSvg } from '../types/svg';
 
-const svgo = new SVGO({ floatPrecision: 0 });
-
 const optimizeSvg = async (svg: string, args = {}): Promise<OptimizedSvg> => {
-  return svgo.optimize(svg, args);
+  return optimize(svg, args);
 };
 
 export default optimizeSvg;
