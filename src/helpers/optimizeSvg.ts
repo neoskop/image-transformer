4fc384e321919c5b9f022a3c96e23102add6a18f
Holger Evers <evers@neoskop.de>
2022-06-29 12:39:29 +0200

    chore(): update all dependencies

--- src/helpers/optimizeSvg.ts
+++ src/helpers/optimizeSvg.ts
@@ -1,10 +1,8 @@
-import SVGO from 'svgo';
+const { optimize } = require('svgo');
 import { OptimizedSvg } from '../types/svg';
 
-const svgo = new SVGO({ floatPrecision: 0 });
-
 const optimizeSvg = async (svg: string, args = {}): Promise<OptimizedSvg> => {
-  return svgo.optimize(svg, args);
+  return optimize(svg, args);
 };
 
 export default optimizeSvg;
