819b225b1529642da7cf14a7ead82f06d7030178
Holger Evers <evers@neoskop.de>
2022-06-29 16:16:27 +0200

    chore(): remove log package

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,5 @@
-import chalk from 'chalk';
-
 const logger = (message: string): void => {
-  console.log(
-    `${chalk
-      .bgRgb(62, 36, 71)
-      .gray('[@neoskop/image-generator]')} ${chalk.white(message)}`
-  );
+  console.log(`[@neoskop/image-generator] ${message}`);
 };
 
 export default logger;
